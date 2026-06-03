9a3f32750275af2e610ee156ac548ba8e732ce4d
MatiasVerschaeHerrera <matias.verschae.h@mail.pucv.cl>
2026-06-03 19:26:56 +0000

    hola

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
+// bibliotecas importadas
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// archivos desde la carpeta tdas
+#include "tdas/extra.h"
+#include "tdas/heap.h"
+#include "tdas/list.h"
+#include "tdas/map.h"
+#include "tdas/multimap.h"
+#include "tdas/queue.h"
+#include "tdas/stack.h"
 
 int main(){
 
